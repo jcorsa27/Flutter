8039e8bf4ca5c8a6a9bfe92c84d233948623b12a
jcorsa27 <64654155+jcorsa27@users.noreply.github.com>
2021-08-14 11:14:46 -0500

    Create basico.dart

---
+++ basico.dart
@@ -0,0 +1,15 @@
+void main() {
+  for (int i = 0; i < 5; i++) {
+    print('hello ${i + 1}');
+  }
+  var num_entero = 15;
+  var nombre = 'Jean Carlos';
+  double num_flotante = 15.34;
+  print(num_entero + num_flotante);
+  bool valor = false;
+  if(valor == true){
+    print('Valor de Positivo');
+  }else{
+    print('Valor de Falso');
+  };
+}
